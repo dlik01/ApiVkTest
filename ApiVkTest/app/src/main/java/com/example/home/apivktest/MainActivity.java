49c642533cef6921208827992346ab8916212331
dlik <sber-9191@yandex.ru>
2018-03-01 03:20:48 +0500

    чучуть переработал класс Main

--- ApiVkTest/app/src/main/java/com/example/home/apivktest/MainActivity.java
+++ ApiVkTest/app/src/main/java/com/example/home/apivktest/MainActivity.java
@@ -6,6 +6,7 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.provider.Settings;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -15,74 +16,145 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
-import com.vk.sdk.VKAccessToken;
-import com.vk.sdk.VKCallback;
-import com.vk.sdk.VKScope;
-import com.vk.sdk.VKSdk;
-import com.vk.sdk.VKUIHelper;
-import com.vk.sdk.api.VKApi;
-import com.vk.sdk.api.VKApiConst;
-import com.vk.sdk.api.VKError;
-import android.view.View;
-import com.vk.sdk.api.VKParameters;
-import com.vk.sdk.api.VKRequest;
-import com.vk.sdk.api.VKResponse;
-import com.vk.sdk.api.model.VKList;
-import com.vk.sdk.util.VKUtil;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Arrays;
 
 public class MainActivity extends Activity {
     //Список получаемых данных
-    private String[] score = new String[]{VKScope.FRIENDS};
+    //private String[] score = {"1", "2", "3", "4", "5", "6", "7", "8"};
+    private String[] arrayStoryMovies = new String[0];
+    private String[] arrayUrlImage = new String[0];
+    private ArrayAdapter<String> arrayAdapterStory = new ArrayAdapter<String>(MainActivity.this, R.layout.row, R.id.tView);
+
     private ListView listView;
 
     public boolean notific = false;                         //для работы с уведомлениями
     private static final int NOTIFY_ID = 101;
 
 
+    private class ParseTask extends AsyncTask<Void, Void, String>{
+
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
+        String resultJson = "";
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            //получаем данные с внешнего рессурса
+            try{
+                URL url = new URL("https://jsonparsingdemo-cec5b.firebaseapp.com/jsonData/moviesData.txt");
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine())!=null){
+                    buffer.append(line);
+                }
+                resultJson = buffer.toString();
+
+            }catch (Exception e){
+                Toast.makeText(MainActivity.this, "что-то пошло не так", Toast.LENGTH_SHORT).show();
+
+            }
+
+            return resultJson;
+        }
+
+        @Override
+        protected void onPostExecute(String strJson){
+            super.onPostExecute(strJson);
+
+            JSONObject dataJsonObj = null;
+            String nameMovies = "";
+
+            //ArrayAdapter<String> arrayAdapterStory = new ArrayAdapter<String>(MainActivity.this, R.layout.row, R.id.tView);
+
+            try{
+
+                dataJsonObj = new JSONObject(strJson);
+                JSONArray movies = dataJsonObj.getJSONArray("movies");
+
+                for (int i = 0; i < movies.length(); i++) {
+                    JSONObject film = movies.getJSONObject(i);
+
+                    //JSONObject contacts = friend.getJSONObject("contacts");
+
+                    String movie = film.getString("movie");
+                    String year = film.getString("year");
+                    String story = film.getString("story");
+                    String urlImg = film.getString("image");
+                    //arrayUrlImage[i] = urlImg;
+                    //arrayStoryMovies[i] = story;
+                    arrayAdapterStory.add(story);
+
+
+                }
+                //listView.setAdapter(arrayAdapterStory);
+
+            }
+            catch (Exception e){
+
+            }
+
+
+            //ArrayAdapter<String> arrayAdapterName = new ArrayAdapter<String>()
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new ParseTask().execute();
 
-        VKSdk.login(this, score);
+
 
         listView = (ListView)findViewById(R.id.listView);
+        //new ParseTask();
 
         Intent notificationIntent = new Intent(this, MainActivity.class);
-        final PendingIntent contentIntent = PendingIntent.getActivity(this,0, notificationIntent,
-                PendingIntent.FLAG_CANCEL_CURRENT);
+        final PendingIntent contentIntent = PendingIntent.getActivity(this,0, notificationIntent, PendingIntent.FLAG_CANCEL_CURRENT);
         final NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
         final NotificationManager notificationManager =
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        //ArrayAdapter<String> arrayAdapterName = new ArrayAdapter<String>(MainActivity.this, R.layout.row, R.id.tView, arrayStoryMovies);
+
+        //ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(MainActivity.this, R.layout.row, R.id.tView, arrayUser);
+        listView.setAdapter(arrayAdapterStory);
 
 
 
-        VKRequest request = VKApi.friends().get(VKParameters.from(VKApiConst.FIELDS, "first_name, last_name"));
+    }
 
-        request.executeWithListener(new VKRequest.VKRequestListener() {
-            @Override
-            public void onComplete(VKResponse response) {
+}
+/* метод onItemClick для появления уведомлений.
 
-                super.onComplete(response);
 
-                final VKList list = (VKList)response.parsedModel;
-                ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(MainActivity.this,
+ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(MainActivity.this,
                         R.layout.row, R.id.tView, list);
 
                 listView.setAdapter(arrayAdapter);
 
-                listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                    @Override
-                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                         String name = String.valueOf(list.get(position));
-                        //String text = String.format("Name: %s; Id: %s", list.get(position), position);
-                        //Toast.makeText(MainActivity.this, text, Toast.LENGTH_SHORT).show();
                         if (notific == false) {
                             notific = true;
                             builder.setContentIntent(contentIntent)
+                                    //иконка для уведомления
                                     .setSmallIcon(R.mipmap.ic_persons)
                                      // Заголовок уведомления
                                     .setContentTitle("Напоминание")
@@ -104,27 +176,6 @@ public class MainActivity extends Activity {
                 });
 
 
+                private String[] score = {"1", "2", "3", "4", "5", "6", "7", "8"};
 
-            }
-        });
-        Toast.makeText(getApplicationContext(), "Вы на равильном пути", Toast.LENGTH_SHORT).show();
-
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (!VKSdk.onActivityResult(requestCode, resultCode, data, new VKCallback<VKAccessToken>() {
-            @Override
-            public void onResult(VKAccessToken res) {
-
-
-            }
-            @Override
-            public void onError(VKError error) {
-                Toast.makeText(getApplicationContext(), "Что-то пошло не так", Toast.LENGTH_SHORT).show();
-            }
-        })) {
-            super.onActivityResult(requestCode, resultCode, data);
-        }
-    }
-}
+ */
