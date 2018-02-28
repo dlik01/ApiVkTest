7481154662a8b938a38edfa09b374f0a2e92e4f3
dlik <sber-9191@yandex.ru>
2018-03-01 03:23:20 +0500

    Добавил класс для управлением разметкой

---
+++ ApiVkTest/app/src/main/java/com/example/home/apivktest/CustomList.java
@@ -0,0 +1,30 @@
+package com.example.home.apivktest;
+
+/**
+ * Created by home on 01.03.2018.
+ */
+
+public class CustomList {
+    private String image;
+    private String story;
+
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getStory() {
+        return story;
+    }
+
+    public void setStory(String story) {
+        this.story = story;
+    }
+
+
+}
+
