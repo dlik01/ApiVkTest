6a2aec1989fe2b445a9da7260f95d8b00abb990b
dlik <sber-9191@yandex.ru>
2018-03-01 13:11:09 +0500

    для удобства переместил некоторые методы,и добавил загрузку элементов с сайта https://jsonparsingdemo-cec5b.firebaseapp.com/jsonData/moviesData.txt поставил временные уведомления.

--- ApiVkTest/app/src/main/java/com/example/home/apivktest/MainActivity.java
+++ ApiVkTest/app/src/main/java/com/example/home/apivktest/MainActivity.java
@@ -1,20 +1,30 @@
 package com.example.home.apivktest;
 
 import android.app.Activity;
-import android.app.ListActivity;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.AsyncTask;
-import android.provider.Settings;
 import android.support.v4.app.NotificationCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.gson.Gson;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.FailReason;
+import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -24,95 +34,23 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+
+
+
 
 public class MainActivity extends Activity {
     //Список получаемых данных
-    //private String[] score = {"1", "2", "3", "4", "5", "6", "7", "8"};
-    private String[] arrayStoryMovies = new String[0];
-    private String[] arrayUrlImage = new String[0];
-    private ArrayAdapter<String> arrayAdapterStory = new ArrayAdapter<String>(MainActivity.this, R.layout.row, R.id.tView);
-
-    private ListView listView;
 
     public boolean notific = false;                         //для работы с уведомлениями
     private static final int NOTIFY_ID = 101;
 
 
-    private class ParseTask extends AsyncTask<Void, Void, String>{
-
-        HttpURLConnection urlConnection = null;
-        BufferedReader reader = null;
-        String resultJson = "";
-
-        @Override
-        protected String doInBackground(Void... voids) {
-            //получаем данные с внешнего рессурса
-            try{
-                URL url = new URL("https://jsonparsingdemo-cec5b.firebaseapp.com/jsonData/moviesData.txt");
-                urlConnection = (HttpURLConnection) url.openConnection();
-                urlConnection.setRequestMethod("GET");
-                urlConnection.connect();
-
-                InputStream inputStream = urlConnection.getInputStream();
-                StringBuffer buffer = new StringBuffer();
-
-                reader = new BufferedReader(new InputStreamReader(inputStream));
-
-                String line;
-                while ((line = reader.readLine())!=null){
-                    buffer.append(line);
-                }
-                resultJson = buffer.toString();
-
-            }catch (Exception e){
-                Toast.makeText(MainActivity.this, "что-то пошло не так", Toast.LENGTH_SHORT).show();
-
-            }
-
-            return resultJson;
-        }
-
-        @Override
-        protected void onPostExecute(String strJson){
-            super.onPostExecute(strJson);
-
-            JSONObject dataJsonObj = null;
-            String nameMovies = "";
-
-            //ArrayAdapter<String> arrayAdapterStory = new ArrayAdapter<String>(MainActivity.this, R.layout.row, R.id.tView);
-
-            try{
-
-                dataJsonObj = new JSONObject(strJson);
-                JSONArray movies = dataJsonObj.getJSONArray("movies");
-
-                for (int i = 0; i < movies.length(); i++) {
-                    JSONObject film = movies.getJSONObject(i);
-
-                    //JSONObject contacts = friend.getJSONObject("contacts");
-
-                    String movie = film.getString("movie");
-                    String year = film.getString("year");
-                    String story = film.getString("story");
-                    String urlImg = film.getString("image");
-                    //arrayUrlImage[i] = urlImg;
-                    //arrayStoryMovies[i] = story;
-                    arrayAdapterStory.add(story);
-
-
-                }
-                //listView.setAdapter(arrayAdapterStory);
-
-            }
-            catch (Exception e){
-
-            }
-
-
-            //ArrayAdapter<String> arrayAdapterName = new ArrayAdapter<String>()
-        }
-    }
+    private ListView listView;
 
 
     @Override
@@ -122,60 +60,193 @@ public class MainActivity extends Activity {
         new ParseTask().execute();
 
 
+        listView = (ListView) findViewById(R.id.listView);
 
-        listView = (ListView)findViewById(R.id.listView);
-        //new ParseTask();
+        DisplayImageOptions defaultOptions = new DisplayImageOptions.Builder()
+                .cacheInMemory(true)
+                .cacheOnDisk(true)
+                .build();
+        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(getApplicationContext())
+                .defaultDisplayImageOptions(defaultOptions)
+                .build();
+        ImageLoader.getInstance().init(config);
 
+
+        //подготавливаем переменные для уведомлений
         Intent notificationIntent = new Intent(this, MainActivity.class);
         final PendingIntent contentIntent = PendingIntent.getActivity(this,0, notificationIntent, PendingIntent.FLAG_CANCEL_CURRENT);
         final NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
         final NotificationManager notificationManager =
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        //ArrayAdapter<String> arrayAdapterName = new ArrayAdapter<String>(MainActivity.this, R.layout.row, R.id.tView, arrayStoryMovies);
-
-        //ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(MainActivity.this, R.layout.row, R.id.tView, arrayUser);
-        listView.setAdapter(arrayAdapterStory);
 
 
+        listView = (ListView)findViewById(R.id.listView);
+
+        // метод onItemClick для появления уведомлений.
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+
+
+                if (notific == false) {
+                    notific = true;
+                            builder.setContentIntent(contentIntent)
+                            //иконка для уведомления
+                            .setSmallIcon(R.mipmap.ic_film_round)
+                            // Заголовок уведомления
+                            .setContentTitle("Напоминание")
+                            // Текст уведомления
+                            .setContentText("Вы хотели посмотреть этот фильм")
+                            // текст в строке состояния
+                            .setTicker("Историю фильма прочитайте сами ")
+                            .setWhen(System.currentTimeMillis())
+                            .setAutoCancel(true);
+
+                    notificationManager.notify(NOTIFY_ID, builder.build());
+                }
+                else {
+                    notific = false;
+                    notificationManager.cancel(NOTIFY_ID);
+                }
+
+
+
+
+
+            }
+        });
+
+    }
+
+    private class ParseTask extends AsyncTask<String, String, List<CustomList> > {
+
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
+
+
+
+        @Override
+        protected List<CustomList> doInBackground(String... params) {
+            // получаем данные с внешнего ресурса
+            String resultJson = null;
+            List<CustomList> movieList = null;
+            try {
+                URL url = new URL("https://jsonparsingdemo-cec5b.firebaseapp.com/jsonData/moviesData.txt");
+
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.connect();
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line);
+                }
+                resultJson = buffer.toString();
+
+                JSONObject parentObject = new JSONObject(resultJson);
+                JSONArray parentArray = parentObject.getJSONArray("movies");
+
+                movieList = new ArrayList<>();
+
+                Gson gson = new Gson();
+                for (int i = 0; i < parentArray.length(); i++) {
+
+                    JSONObject finalObject = parentArray.getJSONObject(i);
+                    CustomList customList = gson.fromJson(finalObject.toString(), CustomList.class);
+
+                    movieList.add(customList);
+                }
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return movieList;
+        }
+
+        @Override
+        protected void onPostExecute(final List<CustomList> strJson) {
+            super.onPostExecute(strJson);
+
+            if (strJson != null){
+                MovieAdapter adapter = new MovieAdapter(getApplicationContext(),R.layout.row, strJson);
+                listView.setAdapter(adapter);
+            }
+            else {
+                Toast.makeText(getApplicationContext(), "не удалось получить данные с сервера", Toast.LENGTH_SHORT).show();
+            }
+
+        }
+    }
+    public class MovieAdapter extends ArrayAdapter {
+
+        private List<CustomList> movieModelList;
+        private int resource;
+        private LayoutInflater inflater;
+        public MovieAdapter(Context context, int resource, List<CustomList> objects) {
+            super(context, resource, objects);
+            movieModelList = objects;
+            this.resource = resource;
+            inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            ViewHolder holder = null;
+
+            if(convertView == null){
+                holder = new ViewHolder();
+                convertView = inflater.inflate(resource, null);
+                holder.ivMovieIcon = (ImageView)convertView.findViewById(R.id.imageView2);
+                holder.tvStory = (TextView)convertView.findViewById(R.id.tView);
+                convertView.setTag(holder);
+            } else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+
+
+            // Это нужно для отображения картинки
+            final ViewHolder finalHolder = holder;
+            ImageLoader.getInstance().displayImage(movieModelList.get(position).getImage(), holder.ivMovieIcon, new ImageLoadingListener() {
+                @Override
+                public void onLoadingStarted(String imageUri, View view) {
+                    finalHolder.ivMovieIcon.setVisibility(View.INVISIBLE);
+                }
+
+                @Override
+                public void onLoadingFailed(String imageUri, View view, FailReason failReason) {
+                    finalHolder.ivMovieIcon.setVisibility(View.INVISIBLE);
+                }
+
+                @Override
+                public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+                    finalHolder.ivMovieIcon.setVisibility(View.VISIBLE);
+                }
+
+                @Override
+                public void onLoadingCancelled(String imageUri, View view) {
+                    finalHolder.ivMovieIcon.setVisibility(View.INVISIBLE);
+                }
+            });
+
+
+            holder.tvStory.setText(movieModelList.get(position).getStory());
+            return convertView;
+        }
+
+
+        class ViewHolder{
+            private ImageView ivMovieIcon;
+            private TextView tvStory;
+        }
 
     }
 
 }
-/* метод onItemClick для появления уведомлений.
 
-
-ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(MainActivity.this,
-                        R.layout.row, R.id.tView, list);
-
-                listView.setAdapter(arrayAdapter);
-
-public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                        String name = String.valueOf(list.get(position));
-                        if (notific == false) {
-                            notific = true;
-                            builder.setContentIntent(contentIntent)
-                                    //иконка для уведомления
-                                    .setSmallIcon(R.mipmap.ic_persons)
-                                     // Заголовок уведомления
-                                    .setContentTitle("Напоминание")
-                                    // Текст уведомления
-                                    .setContentText("Вы хотели посмотреть " + name)
-                                     // текст в строке состояния
-                                    .setTicker("У вас с друзьях" + name)
-                                    .setWhen(System.currentTimeMillis())
-                                    .setAutoCancel(true);
-
-                            notificationManager.notify(NOTIFY_ID, builder.build());
-                        }
-                        else {
-                            notific = false;
-                            notificationManager.cancel(NOTIFY_ID);
-                        }
-
-                    }
-                });
-
-
-                private String[] score = {"1", "2", "3", "4", "5", "6", "7", "8"};
-
- */
