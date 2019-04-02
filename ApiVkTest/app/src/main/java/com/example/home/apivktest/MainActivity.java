0d3f48a18bdc4e353fa639a4df21b3d49d7bf20c
berezin_vv <vnc1sx91>
2019-04-02 23:28:48 +0500

    Добавление меню

--- ApiVkTest/app/src/main/java/com/example/home/apivktest/MainActivity.java
+++ ApiVkTest/app/src/main/java/com/example/home/apivktest/MainActivity.java
@@ -1,21 +1,31 @@
 package com.example.home.apivktest;
 
-import android.app.Activity;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.AsyncTask;
-import android.support.v4.app.NotificationCompat;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -36,32 +46,22 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
-
-
-
-
-
-
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener{
     //Список получаемых данных
 
     public boolean notific = false;                         //для работы с уведомлениями
     private static final int NOTIFY_ID = 101;
-
-
     private ListView listView;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         new ParseTask().execute();
-
-
-        listView = (ListView) findViewById(R.id.listView);
-
+        listView = findViewById(R.id.listView);
         DisplayImageOptions defaultOptions = new DisplayImageOptions.Builder()
                 .cacheInMemory(true)
                 .cacheOnDisk(true)
@@ -70,8 +70,13 @@ public class MainActivity extends Activity {
                 .defaultDisplayImageOptions(defaultOptions)
                 .build();
         ImageLoader.getInstance().init(config);
-
-
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+//            Objects.requireNonNull(getSupportActionBar()).setTitle("My custom toolbar!");
+//        }
+//        setSupportActionBar(toolbar);
+//        toolbar.setTitle(R.string.hellow);
+        setupNavigationDrawer(toolbar);
         //подготавливаем переменные для уведомлений
         Intent notificationIntent = new Intent(this, MainActivity.class);
         final PendingIntent contentIntent = PendingIntent.getActivity(this,0, notificationIntent, PendingIntent.FLAG_CANCEL_CURRENT);
@@ -79,17 +84,13 @@ public class MainActivity extends Activity {
         final NotificationManager notificationManager =
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
-
-        listView = (ListView)findViewById(R.id.listView);
+        listView = findViewById(R.id.listView);
 
         // метод onItemClick для появления уведомлений.
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-
-
-
                 if (notific == false) {
                     notific = true;
                             builder.setContentIntent(contentIntent)
@@ -103,29 +104,87 @@ public class MainActivity extends Activity {
                             .setTicker("Историю фильма прочитайте сами ")
                             .setWhen(System.currentTimeMillis())
                             .setAutoCancel(true);
-
                     notificationManager.notify(NOTIFY_ID, builder.build());
                 }
                 else {
                     notific = false;
                     notificationManager.cancel(NOTIFY_ID);
                 }
-
-
-
-
-
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        switch (id){
+            case R.id.action_add:
+                Toast.makeText(this, getString(R.string.add), Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.action_settings:
+                Toast.makeText(this, R.string.press_settings, Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.action_clear:
+                Toast.makeText(this, getString(R.string.clear), Toast.LENGTH_SHORT).show();
+                break;
+            default:Toast.makeText(this, R.string.default_text, Toast.LENGTH_SHORT).show();
+        }
+
+        return super.onOptionsItemSelected(item);
 
     }
 
-    private class ParseTask extends AsyncTask<String, String, List<CustomList> > {
+    private void setupNavigationDrawer(Toolbar toolbar) {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        setContentView(R.layout.activity_main);
 
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        registerForContextMenu(findViewById(R.id.listView));
+    }
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.main) {
+            Toast.makeText(this, R.string.text_about_developer, Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.contacts) {
+            Toast.makeText(this, R.string.contacts, Toast.LENGTH_SHORT).show();
+        }
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    private class ParseTask extends AsyncTask<String, String, List<CustomList> > {
         HttpURLConnection urlConnection = null;
         BufferedReader reader = null;
-
-
 
         @Override
         protected List<CustomList> doInBackground(String... params) {
@@ -134,35 +193,25 @@ public class MainActivity extends Activity {
             List<CustomList> movieList = null;
             try {
                 URL url = new URL("https://jsonparsingdemo-cec5b.firebaseapp.com/jsonData/moviesData.txt");
-
                 urlConnection = (HttpURLConnection) url.openConnection();
                 urlConnection.connect();
                 InputStream inputStream = urlConnection.getInputStream();
                 StringBuffer buffer = new StringBuffer();
-
                 reader = new BufferedReader(new InputStreamReader(inputStream));
-
                 String line;
                 while ((line = reader.readLine()) != null) {
                     buffer.append(line);
                 }
                 resultJson = buffer.toString();
-
                 JSONObject parentObject = new JSONObject(resultJson);
                 JSONArray parentArray = parentObject.getJSONArray("movies");
-
                 movieList = new ArrayList<>();
-
                 Gson gson = new Gson();
                 for (int i = 0; i < parentArray.length(); i++) {
-
                     JSONObject finalObject = parentArray.getJSONObject(i);
                     CustomList customList = gson.fromJson(finalObject.toString(), CustomList.class);
-
                     movieList.add(customList);
                 }
-
-
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -180,7 +229,6 @@ public class MainActivity extends Activity {
             else {
                 Toast.makeText(getApplicationContext(), "не удалось получить данные с сервера", Toast.LENGTH_SHORT).show();
             }
-
         }
     }
     public class MovieAdapter extends ArrayAdapter {
@@ -197,9 +245,7 @@ public class MainActivity extends Activity {
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-
             ViewHolder holder = null;
-
             if(convertView == null){
                 holder = new ViewHolder();
                 convertView = inflater.inflate(resource, null);
@@ -209,8 +255,6 @@ public class MainActivity extends Activity {
             } else {
                 holder = (ViewHolder) convertView.getTag();
             }
-
-
             // Это нужно для отображения картинки
             final ViewHolder finalHolder = holder;
             ImageLoader.getInstance().displayImage(movieModelList.get(position).getImage(), holder.ivMovieIcon, new ImageLoadingListener() {
@@ -239,7 +283,6 @@ public class MainActivity extends Activity {
             holder.tvStory.setText(movieModelList.get(position).getStory());
             return convertView;
         }
-
 
         class ViewHolder{
             private ImageView ivMovieIcon;
